860b1bd701602f01ba37a285f57f019a6b75c993
petrobubka <76389693+petrobubka@users.noreply.github.com>
2023-04-22 22:55:52 +0300

    Update script1.sh
    upd

--- script1.sh
+++ script1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd /home/grid/testbed/tb410/radixsort-main
+cd /home/grid/testbed/tb117/vec_samples/src
 ml icc
-for i in {1..3};do
-scr=$(date +"$i")
-icc -0$i c.cpp -o try$scr
-echo
-echo "0"$i
-time ./try$scr
+arr=( "avx" "sse4.2" "sse4.1" "sse3" "sse2" "ssse3")
+for j in "${arr[@]}"; do
+for i in {1..3}; do
+icc -O$i -m$j F1.c -o try
+echo "$j $i:"
+time ./try
+done
 done
 ~
