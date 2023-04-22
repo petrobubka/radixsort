d225c5d8ac5b48957f83258f75b048b3eaadb911
petrobubka <76389693+petrobubka@users.noreply.github.com>
2023-04-22 18:53:23 +0300

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -30,7 +30,7 @@ void radixSort(int *array, int n) {
 }
 
 int main() {
-    int count = 400000;
+    int count = 40000;
     int array[count];
     int n = sizeof(array) / sizeof(array[0]);
     for (int j = 0; j < 2000; j++) {
