9b825a0e58d4fa276eaab89fc869b4337901c3f1
petrobubka <76389693+petrobubka@users.noreply.github.com>
2023-04-22 22:32:44 +0300

    Update script1.sh
    fix3

--- script1.sh
+++ script1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/grid/testbed/tb410/radixsort-main
 ml icc
-for 0 in {1..3};do
-scr=_%(date +%A_%H_%M_$S_$N)
-icc -0$0 c.cpp -o try&scr
+for i in {1..3};do
+scr=$(date +"$i")
+icc -0$i c.cpp -o try&scr
 echo
-echo "0"$0
+echo "0"$i
 time ./try$scr
 done
 ~
