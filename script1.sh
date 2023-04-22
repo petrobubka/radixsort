ca6b871c84be81776de184357a9932afc18d090e
petrobubka <76389693+petrobubka@users.noreply.github.com>
2023-04-22 23:06:29 +0300

    Update script1.sh
    fix5

--- script1.sh
+++ script1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /home/grid/testbed/tb117/vec_samples/src
+cd /home/grid/testbed/tb410/radixsort-main
 ml icc
 arr=( "avx" "sse4.2" "sse4.1" "sse3" "sse2" "ssse3")
 for j in "${arr[@]}"; do
 for i in {1..3}; do
-icc -O$i -m$j F1.c -o try
+icc -O$i -m$j c.cpp -o try
 echo "$j $i:"
 time ./try
 done
