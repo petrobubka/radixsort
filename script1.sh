143a15a134bfccd2ca0b01ec25d9de40ef06eea7
petrobubka <76389693+petrobubka@users.noreply.github.com>
2023-04-23 17:46:29 +0300

    Update script1.sh
    final 1

--- script1.sh
+++ script1.sh
@@ -4,10 +4,12 @@ ml icc
 arr=( "avx" "sse4.2" "sse4.1" "sse3" "sse2" "ssse3")
 for j in "${arr[@]}"; do
 for i in {1..3}; do
-name=date +%T
-icc -O$i -m$j c.cpp -o try-$name
-echo "$j $i:"
-time ./try-$name
+name1=$(date +%M)
+name2=$(date +%S)
+icc -O$i -m$j c.cpp -o try-$name1-$name2
+echo
+echo $j " " $i
+time ./try-$name1-$name2
 done
 done
 ~
